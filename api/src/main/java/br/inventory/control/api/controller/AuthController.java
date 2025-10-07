8e089bf620fd985fc9a1bdea71e9488fb13beb1b
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-10-07 20:06:30 -0300

    feat: adds refresh token logic to login route

--- api/src/main/java/br/inventory/control/api/controller/AuthController.java
+++ api/src/main/java/br/inventory/control/api/controller/AuthController.java
@@ -1,8 +1,10 @@
 package br.inventory.control.api.controller;
 
-import br.inventory.control.api.dto.JwtResponseDTO;
-import br.inventory.control.api.dto.LoginRequestDTO;
+import br.inventory.control.api.config.security.JwtService;
+import br.inventory.control.api.dto.*;
+import br.inventory.control.api.model.RefreshToken;
 import br.inventory.control.api.service.AuthService;
+import br.inventory.control.api.service.RefreshTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,12 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtService jwtService;
 
-    @Operation(summary = "Authenticate user", description = "Authenticates a user and returns a JWT token.")
+    @Operation(summary = "Authenticate user", description = "Authenticates a user and returns an access and refresh JWT token.")
     @ApiResponse(responseCode = "200", description = "Authentication successful")
     @ApiResponse(responseCode = "401", description = "Invalid credentials")
     @PostMapping("/login")
-    public ResponseEntity<JwtResponseDTO> login(@RequestBody LoginRequestDTO request) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
         return ResponseEntity.ok(authService.login(request));
+    }
+
+    @Operation(summary = "Refresh access token", description = "Generates a new access token using a valid refresh token.")
+    @ApiResponse(responseCode = "200", description = "Access token refreshed successfully")
+    @ApiResponse(responseCode = "403", description = "Invalid refresh token")
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshTokenResponseDTO> refreshToken(@RequestBody RefreshTokenRequestDTO request) {
+        return refreshTokenService.findByToken(request.getRefreshToken())
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String accessToken = jwtService.generateToken(user);
+                    return ResponseEntity.ok(new RefreshTokenResponseDTO(accessToken, request.getRefreshToken()));
+                })
+                .orElseThrow(() -> new RuntimeException("Refresh token is not in database!"));
     }
 }
