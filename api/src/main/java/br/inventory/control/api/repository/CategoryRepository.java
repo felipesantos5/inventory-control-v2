9683b5d861553c7848d01d35467e3cbad35759b7
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-10-09 20:19:54 -0300

    fear: add some methods to repositories

--- api/src/main/java/br/inventory/control/api/repository/CategoryRepository.java
+++ api/src/main/java/br/inventory/control/api/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package br.inventory.control.api.repository;
 import br.inventory.control.api.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByOrderByNameAsc();
 }
